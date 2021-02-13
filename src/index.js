7193fa9cbc33aef5ca7ae1602fe158bb6be6efc6
Saurav Kumar <saurav.primary@gmail.com>
2021-02-14 02:32:31 +0530

    pre-final

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const cors = require('cors')
 const bodyParser = require("body-parser")
-const mongodb = require('./mongodb')
+const mongodb = require('./mongodb') 
 
 const app = express()
 app.use(cors());
@@ -9,47 +9,100 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-const port =   process.env.PORT || 4000
+const port =   process.env.PORT || 8081
 
+
+// Handle to greet to backend server
 app.get('/',(req, res)=>{
     res.send("Welcome to meme-stream backend ...")
 })
+
+// Handle for rendering 100 recent memes
 app.get('/memes',async (req, res)=>{
     var data = await mongodb.getMemeFunction()
-    res.send(data)
+
+    var mid = data.length/2, temp;
+    for(var i=0; i<data.length/2; i++)
+    {
+        temp = data[i];
+        data[i]=data[data.length - i -1]
+        data[data.length - i - 1] = temp
+    }
+    var recentMeme = []
+    for(var i=0; i<data.length; i++)
+        recentMeme[i]= {
+            id : data[i]._id,
+            name : data[i].name,
+            url : data[i].url,
+            caption : data[i].caption
+        }
+
+    data = recentMeme
+
+    res.status(200).send(data)
 })
-app.post('/memes',(req, res)=>{
+
+// Handle for uploading created memes
+app.post('/memes',async (req, res)=>{
+    var months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+    var date = new Date(Date.now())
+    var date = `Posted on ${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`
     var data = {
         name: req.body.name,
         caption : req.body.caption,
-        date : req.body.date,
-        imgURL : req.body.imgURL
+        date : date,
+        url : req.body.url
     }
-    mongodb.addMemeFunction(data)
-    res.send("Set successfull")
+    var response = await mongodb.addMemeFunction(data)
+
+    if(response == '1')
+        res.sendStatus(409).send("Meme already exists. Cannot upload duplicate meme.")
+    else
+        res.status(200).send({id : response})
 })
+
+// Handle for searching memes by ID
 app.get('/memes/:id', async (req, res) =>{
     var meme = await mongodb.getMemesByID( req.params.id)
-    //console.log(meme,req.params.id)
-    res.send(meme)
+    var data 
+    if(meme)
+    {
+        data = meme[0]
+        data = {
+            id : data._id,
+            name : data.name,
+            url : data.url,
+            caption : data.caption
+        }
+        res.status(200).send(data)
+    }
+    res.sendStatus(404).send(data)
 })
-app.post('/memes/delete',async (req,res)=>{
-    //console.log(req.body)
-    var data = await mongodb.deleteMemesByID(req.body.id)
-    res.send(data)
+
+// Handle for deleting memes
+app.delete('/memes/:id',async (req,res)=>{
+    //console.log(req.params)
+    var data = await mongodb.deleteMemesByID(req.params.id)
+    res.status(200).send(data)
 })
-app.post('/memes/edit',async (req,res)=>{
+
+// Handle for updating memes
+app.patch('/memes/:id',async (req,res)=>{
     //console.log(req.body)
+    var months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+    var date = new Date(Date.now())
+    var date = `Updated on ${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`
     var incomingData = {
         caption: req.body.caption,
-        imgURL: req.body.imgURL,
-        date : req.body.date
+        url: req.body.url,
+        date : date
     }
-    var data = await mongodb.updateMemesByID(req.body.id, incomingData)
-    res.send(data)
+    var data = await mongodb.updateMemesByID(`${req.params.id}`, incomingData)
+    res.sendStatus(200).send('Ok')
 })
 
-
+// Handle for assigning port
 app.listen( port , ()=>{
     console.log(`App started at ` + port)
 })
+
--- src/mongodb.js
+++ src/mongodb.js
@@ -1,6 +1,6 @@
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
-const { ObjectId } = require('mongodb')
+const { ObjectId, ObjectID } = require('mongodb')
 const request = require("request")
 const constants= require('../constants')
 
@@ -13,16 +13,6 @@ const mongoConnect = async function(){
         return await MongoClient.connect( connectionURL, {useUnifiedTopology : true, useNewUrlParser : true})
 }
 
-// ***** Function for adding memes *****
-var addMemeFunction = function(data){
-    mongoConnect().then(async client =>{
-        const db= client.db(databaseName)
-        await db.collection('memes').insertOne(data);
-        client.close()
-    }).catch(err => console.log(err))
-
-    return "Memes added successfully !!!"
-}
 // ***** Function for getting memes *****
 var getMemeFunction = async function(){
     var memes
@@ -36,6 +26,29 @@ var getMemeFunction = async function(){
     return memes
 }
 
+// ***** Function for adding memes *****
+var addMemeFunction = async function(data){
+    var response = await getMemeFunction(),check = 0
+    for(var i=0; i<response.length; i++)
+    {
+        if((response[i].caption == data.caption) && (response[i].name == data.name) && (response[i].imgURL == data.imgURL))
+            check++
+    }
+    if(check)
+    {
+        return "1"
+    }
+    else
+    {
+        var id
+        await mongoConnect().then(async client =>{
+            const db= client.db(databaseName)
+            await db.collection('memes').insertOne(data).then(response => id = response.insertedId).catch(err =>console.log(err));
+            client.close()
+        }).catch(err => console.log(err))
+        return id
+    }
+}
 // ***** Function for getting memes by ID *****
 var getMemesByID = async function(id){
     var memes
@@ -59,16 +72,17 @@ var deleteMemesByID = async function(id){
 
 // ***** Function for updating memes *****
 var updateMemesByID = async function(id, data){
-    //console.log(data)
+    //console.log(id)
+    var err1
     await mongoConnect().then(async client =>{
         const db= client.db(databaseName)
-        await db.collection('memes').updateOne({_id : ObjectId(id)},{$set : {
+        await db.collection('memes').updateOne({_id : ObjectID(id)},{$set : {
             caption: data.caption,
             imgURL: data.imgURL,
             date : data.date
         }})
         client.close()
-    }).catch(err => console.log(err))
+    }).catch(err => console.log('ok',err))
     return 'Updated successfully'
 }
 
